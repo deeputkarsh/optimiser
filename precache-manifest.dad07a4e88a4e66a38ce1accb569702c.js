338bec025e02a3ee56971da8eb1d1603567693d8
Utkarsh Deep <utkarshdeep@outlook.com>
2019-12-21 01:00:28 +0530

    Updates

---
+++ precache-manifest.dad07a4e88a4e66a38ce1accb569702c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcfb55a0d384506d110dfaf12ad8cc3f",
+    "url": "/optimiser/index.html"
+  },
+  {
+    "revision": "5304dd35ba211fc538a1",
+    "url": "/optimiser/static/css/main.19c0351a.chunk.css"
+  },
+  {
+    "revision": "5740770b44a7d5f5b72e",
+    "url": "/optimiser/static/js/2.765abfc1.chunk.js"
+  },
+  {
+    "revision": "022c664b82956dcf39adefd5dcb2f586",
+    "url": "/optimiser/static/js/2.765abfc1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5304dd35ba211fc538a1",
+    "url": "/optimiser/static/js/main.a0f80b4a.chunk.js"
+  },
+  {
+    "revision": "caff1deb892020cb0594",
+    "url": "/optimiser/static/js/runtime-main.27d3cbbc.js"
+  },
+  {
+    "revision": "507c0ddc4350f9bdda96b9846f0c0a78",
+    "url": "/optimiser/static/media/404-Error.507c0ddc.png"
+  }
+]);
