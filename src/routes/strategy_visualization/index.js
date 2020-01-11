07bd060ac1d7c1a00997d3da2ddab1665acd0f97
deeputkarsh <utkarshdeep@outlook.com>
2020-01-11 16:14:42 +0530

    bar primary color change

--- src/routes/strategy_visualization/index.js
+++ src/routes/strategy_visualization/index.js
@@ -3,14 +3,14 @@ import { Grid, Paper } from '@material-ui/core'
 import { Bar } from 'react-chartjs-2'
 
 const chartList = [
-  { key: 'WOS', label: 'Weeks Of Supply' },
-  { key: 'erosion', label: 'Erosion' },
-  { key: 'EOH', label: 'Ending On Hand Dollars' },
-  { key: 'EOHUnits', label: 'Ending On Hand Units' },
-  { key: 'avgOBDisc', label: 'Average OB Discount' },
-  { key: 'salesUnits', label: 'Sales Units' },
-  { key: 'COGS', label: 'Cost Of Goods Sold' },
-  { key: 'recoveryRate', label: 'Recovery Rate' }
+  { key: 'WOS', label: 'Weeks Of Supply', primaryColor: a => `rgba(255,99,132,${a})` },
+  { key: 'erosion', label: 'Erosion', primaryColor: a => `rgba(255,132,99,${a})` },
+  { key: 'EOH', label: 'Ending On Hand Dollars', primaryColor: a => `rgba(132,255,99,${a})` },
+  { key: 'EOHUnits', label: 'Ending On Hand Units', primaryColor: a => `rgba(132,99,255,${a})` },
+  { key: 'avgOBDisc', label: 'Average OB Discount', primaryColor: a => `rgba(99,255,132,${a})` },
+  { key: 'salesUnits', label: 'Sales Units', primaryColor: a => `rgba(99,132,255,${a})` },
+  { key: 'COGS', label: 'Cost Of Goods Sold', primaryColor: a => `rgba(255,190,25,${a})` },
+  { key: 'recoveryRate', label: 'Recovery Rate', primaryColor: a => `rgba(255,25,190,${a})` }
 ]
 
 const getChartDataSet = (summaryList, chartItem) => {
@@ -20,17 +20,16 @@ const getChartDataSet = (summaryList, chartItem) => {
     datasets: [
       {
         label: chartItem.label,
-        backgroundColor: chartItem.backgroundColor || 'rgba(255,99,132,0.2)',
-        borderColor: 'rgba(255,99,132,1)',
         borderWidth: 1,
-        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-        hoverBorderColor: 'rgba(255,99,132,1)',
+        backgroundColor: chartItem.primaryColor('0.5'),
+        borderColor: chartItem.primaryColor('1'),
+        hoverBackgroundColor: chartItem.primaryColor('0.8'),
+        hoverBorderColor: chartItem.primaryColor('1'),
         data: summaryList.map(summary => summary.forecastResults[chartItem.key])
       }
     ]
   }
 }
-
 const StrategyVisualization = props => {
   const { summaryList } = props
   return (
